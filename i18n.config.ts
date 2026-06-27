ceedfa8862b5b3912ddfa09c2f2e34e298e0de16
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2026-06-28 03:11:58 +0800

    chore: add namespaces and close account to i18n config

--- i18n.config.ts
+++ i18n.config.ts
@@ -4,9 +4,19 @@ const i18nConfig: I18nConfig = {
   supportedLngs: ["en", "de"],
   fallbackLng: "en",
   defaultNS: "common",
-  ns: ["common", "home"],
-  resourceLoader: (language, namespace) =>
-    import(`./src/i18n/locales/${language}/${namespace}.json`),
+  ns: [
+    "common",
+    "home",
+    "license",
+    "privacy",
+    "terms",
+    "notFound",
+    "closeAccount",
+  ],
+  resourceLoader: (language, namespace) => {
+    const file = namespace.replace(/([a-z0-9])([A-Z])/g, "$1-$2").toLowerCase();
+    return import(`./src/i18n/locales/${language}/${file}.json`);
+  },
 };
 
 export default i18nConfig;
