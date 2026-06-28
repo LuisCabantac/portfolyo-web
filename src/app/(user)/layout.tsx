39414fa014d4960d0c8893ff4c8652caa8bff8f4
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2026-06-29 04:57:35 +0800

    feat: add metadata to explore page

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,4 +1,12 @@
+import { Metadata } from "next";
+
 import Header from "@/components/header";
+
+export const metadata: Metadata = {
+  title: "Explore",
+  description:
+    "Browse and discover developer portfolios on Portfolyo. Explore projects, filter by expertise, and find inspiration from the developer community.",
+};
 
 const RootLayout = async ({
   children,
